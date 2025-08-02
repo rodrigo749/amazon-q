02f375f01c6186b52de1219f52279d0563028cdc
Rodrigo Neves Ottoboni <rodrigonottoboni@outlook.com>
2025-08-02 09:24:08 -0300

    amazon manual invoke

--- AmazonQ_manual_invoke.py
+++ AmazonQ_manual_invoke.py
@@ -1,3 +1,16 @@
 # TODO: Press either Option + C on MacOS or Alt + C on Windows on a new line.
 
 # Function to upload a file to an S3 bucket.
+def upload_file(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
